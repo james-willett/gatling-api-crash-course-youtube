71b21f34bb13070ec2a93f918e77b2af1dfe7198
james-willett <james.willett58@gmail.com>
2023-02-15 08:14:26 +0000

    Adding template scenario and http methods

--- src/test/java/videogamedb/VideoGameDbSimulation.java
+++ src/test/java/videogamedb/VideoGameDbSimulation.java
@@ -11,12 +11,54 @@ public class VideoGameDbSimulation extends Simulation {
     // Http Configuration
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://videogamedb.uk/api")
-            .acceptHeader("application/json");
+            .acceptHeader("application/json")
+            .contentTypeHeader("application/json");
+
+    // HTTP CALLS
+    private static ChainBuilder getAllVideoGames =
+            exec(http("Get all video games")
+                    .get("/videogame"));
+
+    private static ChainBuilder authenticate =
+            exec(http("Authenticate")
+                    .post("/authenticate")
+                    .body(StringBody("{\n" +
+                            "  \"password\": \"admin\",\n" +
+                            "  \"username\": \"admin\"\n" +
+                            "}"))
+                    .check(jmesPath("token").saveAs("jwtToken")));
+
+    private static ChainBuilder createNewGame =
+            exec(http("Create New Game")
+                    .post("/videogame")
+                    .header("Authorization", "Bearer #{jwtToken}")
+                    .body(ElFileBody("bodies/newGameTemplate.json")).asJson());
+
+    private static ChainBuilder getLastPostedGame =
+            exec(http("Get Last Posted Game")
+                    .get("/videogame/1"));
+
+    private static ChainBuilder deleteLastPostedGame =
+            exec(http("Delete Game")
+                    .delete("/videogame/1")
+                    .header("Authorization", "Bearer #{jwtToken}"));
 
     // Scenario Definition
+    // 1. Get all video games
+    // 2. Authenticate with API
+    // 3. Create a new game
+    // 4. Get details of newly created game
+    // 5. Delete newly created game
     private ScenarioBuilder scn = scenario("Video Game DB Stress Test")
-            .exec(http("Get all games")
-                    .get("/videogame"));
+            .exec(getAllVideoGames)
+            .pause(2)
+            .exec(authenticate)
+            .pause(2)
+            .exec(createNewGame)
+            .pause(2)
+            .exec(getLastPostedGame)
+            .pause(2)
+            .exec(deleteLastPostedGame);
 
     // Load Simulation
     {
